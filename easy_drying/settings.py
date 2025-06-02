5dd96148f26e47b0768f1d4c77bdf80598df29a0
tanvirac24 <tanvirac024@gmail.com>
2025-06-02 18:47:15 +0600

    E-mail Verification Setup

--- easy_drying/settings.py
+++ easy_drying/settings.py
@@ -180,6 +180,9 @@ SIMPLE_JWT = {
    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
     'SERIALIZERS':{
        'user_create': 'users.serializers.UserCreateSerializer', 
        'current_user': 'users.serializers.UserSerializer', 
@@ -197,3 +200,10 @@ SWAGGER_SETTINGS = {
       }
    }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS =config('EMAIL_USE_TLS',cast=bool)
+EMAIL_PORT =config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =config('EMAIL_HOST_PASSWORD')
